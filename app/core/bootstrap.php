2df89d20015a948339afeccd05460b089e2c7c92
Dammy Brainlord <dammybrainlord@gmail.com>
2018-01-31 21:39:22 +0100

    Updated file to remove dumped variables

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -4,6 +4,5 @@ use App\Core\App;
 use App\Core\Misc\EventManager;
 
 EventManager::on(App::EVENT_ROUTE_NO_MATCH_FOUND, function($path) {
-    var_dump($_SERVER);
     echo "Page '<b>{$path}</b>' Not found";
 });
