dd92ab309da2954f13ded922522d338b6c3ac268
Dammy Brainlord <dammybrainlord@gmail.com>
2018-03-27 13:07:04 +0100

    Added custom 404 page

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -4,5 +4,5 @@ use App\Core\App;
 use App\Core\Misc\EventManager;
 
 EventManager::on(App::EVENT_ROUTE_NO_MATCH_FOUND, function($path) {
-    echo "Page '<b>{$path}</b>' Not found";
+    return response()->view('404');
 });
