76a8c0f61821e60f6b5d337317d41778c17e4f81
Dammy Brainlord <dammybrainlord@gmail.com>
2018-03-27 10:32:06 +0100

    Re-organized the methods to be modular

--- main/core/Http/Controller.php
+++ main/core/Http/Controller.php
@@ -10,21 +10,53 @@ class Controller
 {
 
     /**
+     * Instance of self
+     *
+     * @var Controller;
+     */
+    private static $_instance;
+
+    /**
      * Controller constructor
      * 
      */
     public function __construct()
     {
+        static::__init__(new Request);
+    }
 
-        #Assign url
-        $request = new Request;
+    /**
+     * Get controller's instance
+     *
+     * @return Controller
+     */
+    public static function getInstance()
+    {
+        if(!static::$_instance) {
+            static::$_instance = new self;
+        }
+        
+        return static::$_instance;
+    }
+
+    /**
+     * Initialize the controller to assign default attributes
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return void
+     */
+    private static function __init__(Request $request)
+    {
         $url = $request->url();
 
         #Assign required variables to view
         ResponseView::multiAssign(
             array(
-                'url' => $url->getUrl(),
-                'current_url' => $url->getFullUrl()
+                '_url' => $url->getUrl(),
+                '_current_url' => $url->getFullUrl(),
+                '_current_path' => $url->getPath(),
+                '_request_method' => $request->getMethod()
             )
         );
     }
