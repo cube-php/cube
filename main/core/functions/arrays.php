892e36cf9fdfacf600c9f4f5b43f1869afbe5aae
Dammy Brainlord <dammybrainlord@gmail.com>
2018-03-27 13:09:10 +0100

    Added array_shuffle, array_get_first and array_get_last methods to the array functions

--- main/core/functions/arrays.php
+++ main/core/functions/arrays.php
@@ -8,6 +8,38 @@
  * ==============================================
  */
 
+ /**
+  * Get the first element of an array
+  *
+  * @param array $arr
+  * @return void
+  */
+function array_get_first(array $arr) {
+    return $arr[0];
+}
+
+/**
+ * Get the last element of an array
+ *
+ * @param array $arr
+ * @return mixed
+ */
+function array_get_last(array $arr) {
+    $index = count($arr) - 1;
+    return $arr[$index];
+}
+
+/**
+ * Shuffle array
+ *
+ * @param array $arr
+ * @return array
+ */
+function array_shuffle(array $arr) {
+    shuffle($arr);
+    return $arr;
+}
+
 /**
  * Wrap value as array if not an array
  * Else return the value itself
@@ -15,8 +47,7 @@
  * @param mixed $var
  * @return array
  */
-function array_wrap($var) : array
-{
+function array_wrap($var) {
     if(!is_array($var)) {
         return [$var];
     }
