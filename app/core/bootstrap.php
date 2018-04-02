1e4ff904d6d72ed38780c7fe392fd4344f6bd86f
Dammy Brainlord <dammybrainlord@gmail.com>
2018-04-02 07:58:24 +0100

    Updated the return value for route's events

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Core\App;
+use App\Core\Http\Request;
 use App\Core\Misc\EventManager;
 
-EventManager::on(App::EVENT_ROUTE_NO_MATCH_FOUND, function($path) {
-    return response()->view('404');
+EventManager::on(App::EVENT_ROUTE_NO_MATCH_FOUND, function(Request $request) {
+    return response()->view('404', [
+        '_path' => $request->url()->getPath(),
+        '_request_method' => $request->getMethod()
+    ]);
 });
