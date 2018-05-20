2a78b7abc877246b2b166e6a0a74ab5a4f532244
Dammy Brainlord <dammybrainlord@gmail.com>
2018-05-20 23:55:47 +0100

    Updated the error page file path

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -17,7 +17,7 @@ use App\Core\Http\Request;
 use App\Core\Misc\EventManager;
 
 EventManager::on(App::EVENT_ROUTE_NO_MATCH_FOUND, function(Request $request) {
-    return response()->view('404', [
+    return response()->view('default.404', [
         '_path' => $request->url()->getPath(),
         '_request_method' => $request->getMethod()
     ]);
