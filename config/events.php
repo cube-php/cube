53352b8ddb38c4a1a284db7a1e19902621676d3d
Salawu Oluwadamilola Ezekiel <damilolaoffical@gmail.com>
2020-01-08 09:31:59 +0100

    chore: App now dispatches an event when a crash occurs in production mode

--- config/events.php
+++ config/events.php
@@ -1,9 +1,14 @@
 <?php
 use App\Core\App;
+use App\Events\AppCrashEvent;
 use App\Events\RouteNotFoundEvent;
 
 return array(
     App::EVENT_ROUTE_NO_MATCH_FOUND => [
         RouteNotFoundEvent::class
+    ],
+
+    App::EVENT_APP_ON_CRASH => [
+        AppCrashEvent::class
     ]
 );
