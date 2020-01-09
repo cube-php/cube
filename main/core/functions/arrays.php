91848460cd166dce61d6fe6aac3efc96b5db2d9a
Salawu Oluwadamilola Ezekiel <damilolaoffical@gmail.com>
2020-01-09 14:22:18 +0100

    [Feature] added a new function for arrays "array_map_class"

--- main/core/functions/arrays.php
+++ main/core/functions/arrays.php
@@ -53,3 +53,18 @@ function array_wrap($var) {
     }
     return $var;
 }
+
+/**
+ * Map array to class
+ *
+ * @param array $array Array to map
+ * @param string $key Key
+ * @param string $class Class name to map
+ * @return array
+ */
+function array_map_class(array $array, $key, $class) {
+    return array_map(function ($item) use ($key, $class) {
+        $item = (object) $item;
+        return new $class($item->{$key});
+    }, $array);
+}
