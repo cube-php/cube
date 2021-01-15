0e76328d67740f00a40102f8ee9b980bf947a3e3
dammynex <damilolaofficial@gmail.com>
2021-01-15 21:57:38 +0100

    chore: optimized and updated how models work

--- main/core/Interfaces/ModelInterface.php
+++ main/core/Interfaces/ModelInterface.php
@@ -8,20 +8,39 @@ use Cube\Modules\Db\DBTable;
 
 interface ModelInterface
 {
-    public static function all(?array $order = null, ?array $opts = null, ?array $fields = null, ?string $map = null);
+    public static function all(?array $order = null, ?array $opts = null);
+    
     public static function createEntry(array $entry);
+    
     public static function delete(): DBDelete;
+
+    public static function find($primary_key);
+    
     public static function findAllBy($field, $value, $order = null, $params = null);
-    public static function findBy($field, $value, array $fields = []);
-    public static function findByPrimaryKey($primary_key, array $fields = []);
+    
+    public static function findBy($field, $value);
+    
+    public static function findByPrimaryKey($primary_key);
+    
     public static function findByPrimaryKeyAndRemove($primary_key);
+    
     public static function findByPrimaryKeyAndUpdate($primary_key, array $update);
+    
     public static function getCount();
+    
     public static function getCountBy($field, $value);
+    
     public static function getCountQuery();
-    public static function getFirst($field = null, array $fields = []);
-    public static function getLast($field = null, array $fields = []);
+    
+    public static function getFirst($field = null);
+    
+    public static function getLast($field = null);
+    
     public static function query(): DBTable;
-    public static function select(?array $fields = null): DBSelect;
-    public static function search($field, $keyword, $limit = null, $offset = null, array $fields = []);
+    
+    public static function select(): DBSelect;
+    
+    public static function search($field, $keyword, $limit = null, $offset = null);
+
+    public static function sum(string $field);
 }
