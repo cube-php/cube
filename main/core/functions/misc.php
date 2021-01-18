b72bfb56e4cf03f6f3963b3ae1fe459db4d14ea8
dammynex <damilolaofficial@gmail.com>
2021-01-18 11:33:26 +0100

    feat: added function to die and dump

--- main/core/functions/misc.php
+++ main/core/functions/misc.php
@@ -17,3 +17,15 @@
 function generate_token($length) {
     return bin2hex(openssl_random_pseudo_bytes($length));
 }
+
+/**
+ * Die Dump
+ *
+ * @param mixed $data
+ * @param boolean $should_die
+ * @return void
+ */
+function dd($data, bool $should_die = true) {
+    var_dump($data);
+    if($should_die) die();
+}
