bb31aae65f912649dc5b66a2c68879ae15413b1f
Damilola <damilolaofficial@gmail.com>
2024-08-15 18:33:47 +0100

    fix: updated database setting

--- config/database.php
+++ config/database.php
@@ -7,59 +7,28 @@
  * Set all database settings
  */
 
-use Cube\Http\Env;
-
 return array(
 
-    /**
-     * Database driver
-     * 
-     * @var string
-     */
-    'driver' => Env::get('db_driver'),
+    'mysql' => array(
+        /** @var string Database Driver */
+        'driver' => env('db_driver'),
 
-    /**
-     * Database host name
-     * 
-     * By default: localhost
-     * This varies depending on web host/server
-     * 
-     * @var string
-     */
-    'hostname' => Env::get('db_hostname'),
+        /** @var string Database host name */
+        'hostname' => env('db_hostname'),
 
-    /**
-     * Database username
-     * 
-     * @var string
-     */
-    'username' => Env::get('db_username'),
+        /** @var string Database username */
+        'username' => env('db_username'),
 
-    /**
-     * Database password
-     * 
-     * @var string
-     */
-    'password' => Env::get('db_password'),
+        /** @var string Database password */
+        'password' => env('db_password'),
 
-    /**
-     * Database table name
-     * 
-     * @var string
-     */
-    'dbname' => Env::get('db_name'),
+        /** @var string Database name */
+        'dbname' => env('db_name'),
 
-    /**
-     * Database port
-     * 
-     * @var int
-     */
-    'port' => Env::get('db_port'),
+        /** @var string Database prot */
+        'port' => env('db_port'),
 
-    /**
-     * Database charset
-     * 
-     * @var string
-     */
-    'charset' => Env::get('db_charset')
+        /** @var string Database character set */
+        'charset' => env('db_charset')
+    )
 );
